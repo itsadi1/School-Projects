6e68b34d45842af67f529a214b7d7c5823e89ce2
Aditya Bajaj <93042011+itsadi1@users.noreply.github.com>
2021-10-23 21:26:02 +0530

    Create simple_alarm.py
    Its just a simple alarm made using datetime module... it

---
+++ simple_alarm.py
@@ -0,0 +1,16 @@
+import datetime,winsound
+hour = input("Enter Hour: ")
+minute = input("Enter minutes: ")
+message = input("Enter Message: ")
+while True:
+    date_time = str(datetime.datetime.now())
+    time_now = str(str(date_time.split(" ")[-1]).split(".")[0]).split(":")
+    hour_now = (time_now[0])
+    min_now  =  (time_now[1])
+    sec_now   =  (time_now[2])
+    if hour_now == hour and min_now == minute:
+        if sec_now == "01":
+            print(message)
+        winsound.Beep(500,1500)
+    else:
+        continue
