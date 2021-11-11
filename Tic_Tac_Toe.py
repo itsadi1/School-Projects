4834325cf490b177b48bd6cf0895219807b1ade5
Aditya Bajaj <93042011+itsadi1@users.noreply.github.com>
2021-11-12 02:10:23 +0530

    Create Tic_Tac_Toe.py
    Input Format :- [column_no.] +space+[row_no.]

---
+++ Tic_Tac_Toe.py
@@ -0,0 +1,112 @@
+m,n,repeated=[],1,False
+
+tic_tac ={(1,1):'-',
+          (1,2):'-',
+          (1,3):'-',
+          (2,1):'-',
+          (2,2):'-',
+          (2,3):'-',
+          (3,1):'-',
+          (3,2):'-',
+          (3,3):'-'}
+
+
+
+while True:
+    
+    if n==1:
+        x = input(f'enter_position_player_{n}: ')
+        n=2
+        a,b = int(x.split()[0]),int(x.split()[1])
+        tic_tac[(a,b)]='x'
+
+    else:
+        x = input(f'enter_position_player_{n}: ')
+        n=1
+        a,b = int(x.split()[0]),int(x.split()[1])
+        tic_tac[(a,b)]='o'
+
+    if a>3 or b >3:
+        print("!!index out of range.!!")
+
+    for i in m:
+
+        if i==x:
+            repeated=True
+            print('values_repeated')
+            quit()
+            break
+
+    if repeated==False:
+        m.append(x)
+
+    for i in range(1,4):
+
+        for j in range(1,4):
+            print(tic_tac.get((i,j)),end='')
+
+            if j<3:
+                print('|',end='')
+
+            else:
+                print('\n',end='')
+
+        if i<3:
+            print('-----')
+
+    diagonalx = ((tic_tac.get((1,1))==tic_tac.get((2,2))==tic_tac.get((3,3))=='x'),
+                 (tic_tac.get((3,3))==tic_tac.get((2,2))==tic_tac.get((1,1))=='x'))
+    
+    diagonalo = ((tic_tac.get((1,1))==tic_tac.get((2,2))==tic_tac.get((3,3))=='o'),
+                 (tic_tac.get((3,3))==tic_tac.get((2,2))==tic_tac.get((1,1))=='o'))
+
+
+    rowx = (tic_tac.get((1,1))==tic_tac.get((1,2))==tic_tac.get((1,3))=='x',
+            tic_tac.get((2,1))==tic_tac.get((2,2))==tic_tac.get((2,3))=='x',
+            tic_tac.get((3,1))==tic_tac.get((3,2))==tic_tac.get((3,3))=='x'
+            )
+
+    colo = (tic_tac.get((1,1))==tic_tac.get((2,1))==tic_tac.get((3,1))=='o',
+            tic_tac.get((1,2))==tic_tac.get((2,2))==tic_tac.get((3,2))=='o',
+            tic_tac.get((1,3))==tic_tac.get((2,3))==tic_tac.get((3,3))=='o'
+            )
+
+    rowo = (tic_tac.get((1,1))==tic_tac.get((1,2))==tic_tac.get((1,3))=='o',
+            tic_tac.get((2,1))==tic_tac.get((2,2))==tic_tac.get((2,3))=='o',
+            tic_tac.get((3,1))==tic_tac.get((3,2))==tic_tac.get((3,3))=='o'
+            )
+
+    colx = (tic_tac.get((1,1))==tic_tac.get((2,1))==tic_tac.get((3,1))=='x',
+            tic_tac.get((1,2))==tic_tac.get((2,2))==tic_tac.get((3,2))=='x',
+            tic_tac.get((1,3))==tic_tac.get((2,3))==tic_tac.get((3,3))=='x '
+            )
+    if any(rowo):
+        print('player_2_won by horizontally')
+        break
+
+    elif any(rowx):
+        print('player_1_won by horizontally')
+        break
+
+    elif any(colo):
+        print('player_2_won by vertically')
+        break
+
+    elif any(colx):
+        print('player_1_won by vertically')
+        break
+
+    elif any(diagonalo):
+        print('player_2_won by diagonally')
+        break
+
+    elif any(diagonalx):
+        print('player_1_won by diagonally')
+        break
+    
+    elif len(m)==9:
+        print('Tie')
+        break
+
+    else:
+        continue
